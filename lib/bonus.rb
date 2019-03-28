81f21eab61a760699841de0dcb1af0a97bc19711
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 00:03:45 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,10 @@ def bonus
   }
 
   #code your solution here:
-#by using the hash:[key]:[key]
+#by using the hash:[key]:[key]:[key] = new_value formula we can now change the value of the hero & heroines status
+
+epic_tragedy
+
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
