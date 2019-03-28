7067a61bb9a73e57dd028e51d74375bd3e4eba03
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 00:01:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-
+#by using the hash:[]
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
